7f49203dc6992722221b14c843318ab86190991c
rnphilp <rnphilp@users.noreply.github.com>
2019-04-24 16:31:18 +0100

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const apiRouter = require('./routes/api-router');
 const { handle400, handle404, handle500 } = require('./errors');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
